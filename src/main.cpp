a556b53656e5d1188310dd6b7f50d6d54bcbb471
Jixian Wang <hellojixian@gmail.com>
2025-05-06 15:56:05 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,24 @@
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  int result = myFunction(2, 3);
+  Serial.print("The result of myFunction(2, 3) is: ");
+  Serial.println(result);
+  Serial.println("Hello, world!");
+  delay(1000); // Wait for 1 second
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
